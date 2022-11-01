795f671599e35eb367ef3b4f69faf3f920c7a0b2
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-01 17:44:24 +0530

    Created a CoronavirusDataService for fetching data.

---
+++ src/main/java/io/atulandjava/org/Coronavirustrackerbeta/service/CoronavirusDataService.java
@@ -0,0 +1,27 @@
+package io.atulandjava.org.Coronavirustrackerbeta.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class CoronavirusDataService {
+
+    /* URL for fetching raw data from a github repo*/
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv";
+
+    @PostConstruct
+    public void fetchData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
