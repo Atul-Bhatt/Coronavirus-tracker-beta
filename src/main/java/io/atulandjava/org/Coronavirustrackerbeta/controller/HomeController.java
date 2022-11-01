2837af74efe97c773459c0218ab0db50a2bce878
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-01 19:02:50 +0530

    Using thymeleaf templating engine.

--- src/main/java/io/atulandjava/org/Coronavirustrackerbeta/controller/HomeController.java
+++ src/main/java/io/atulandjava/org/Coronavirustrackerbeta/controller/HomeController.java
@@ -1,13 +1,15 @@
 package io.atulandjava.org.Coronavirustrackerbeta.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("TestString", "Hello! This is a coronavirus tracking application.");
         return "home";
     }
 }
