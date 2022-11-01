9e7d528fd65d23612cf3e9c62df613151707a736
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-01 17:29:24 +0530

    Initialized a spring project using spring initializer.

---
+++ README.md
@@ -0,0 +1,1 @@
+"#Coronavirus tracking app." 
---
+++ src/test/java/io/atulandjava/org/Coronavirustrackerbeta/CoronavirusTrackerBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.atulandjava.org.Coronavirustrackerbeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
