9129d214d275534152f291f04e14caa470037ef9
Atul Bhatt <atulbhatt61@gmail.com>
2022-11-02 14:27:57 +0530

    Total Reported Cases calculated.

--- src/main/java/io/atulandjava/org/Coronavirustrackerbeta/controller/HomeController.java
+++ src/main/java/io/atulandjava/org/Coronavirustrackerbeta/controller/HomeController.java
@@ -1,10 +1,13 @@
 package io.atulandjava.org.Coronavirustrackerbeta.controller;
 
+import io.atulandjava.org.Coronavirustrackerbeta.model.LocationStats;
 import io.atulandjava.org.Coronavirustrackerbeta.service.CoronavirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -13,7 +16,10 @@ public class HomeController {
 
     @GetMapping
     public String home(Model model){
-        model.addAttribute("locationStats", coronavirusDataService.getLocationStats());
+        List<LocationStats> allStats = coronavirusDataService.getLocationStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getConfirmedCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
         return "home";
     }
 }
